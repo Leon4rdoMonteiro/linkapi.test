430a9c98b895b18cc71ac4910df3d93b97c3925c
Leon4rdoMonteiro <leonardomonteiro.sh@gmail.com>
2021-01-25 21:36:04 -0300

    feat: :goal_net: add method/content negotiation middlewares

--- src/shared/infra/http/middlewares/errorHandler.js
+++ src/shared/infra/http/middlewares/errorHandler.js
@@ -1,19 +1,77 @@
 const AppError = require('../../../errors/AppError');
 
-module.exports = (err, req, res, next) => {
-  if (err instanceof AppError) {
-    const { error, statusCode, message } = err;
+module.exports = {
+  catchNotFound(req, res, next) {
+    next();
 
-    return res.status(statusCode).json({
-      error,
-      message,
+    return res.status(404).json({
+      error: true,
+      status: 404,
+      message: 'Resource not found',
     });
-  }
+  },
 
-  console.error(err);
+  methodNegotiation(req, res, next) {
+    const allowed = ['PUT', 'GET', 'OPTIONS', 'HEAD'];
+    const path = '/v1/opportunities';
 
-  return res.status(500).json({
-    error: true,
-    message: 'Internal server error',
-  });
+    if (!allowed.includes(req.method) && req.path === path) {
+      return res.status(405).json({
+        error: true,
+        status: 405,
+        message: 'Method Not Allowed',
+      });
+    }
+
+    next();
+  },
+
+  contentNegotiation(req, res, next) {
+    const allowed = ['application/json', '*/*'];
+
+    if (!allowed.includes(req.headers.accept)) {
+      return res.status(406).json({
+        error: true,
+        status: 406,
+        message: 'Not Acceptable',
+      });
+    }
+
+    next();
+  },
+
+  contentTypeNegotiation(req, res, next) {
+    const allowed = ['application/json', '*/*'];
+    const contentType = req.headers['content-type'];
+
+    if (contentType && !allowed.includes(contentType)) {
+      return res.status(415).json({
+        error: true,
+        status: 415,
+        message: 'Unsupported Media Type',
+      });
+    }
+
+    next();
+  },
+
+  appError(err, req, res, _) {
+    if (err instanceof AppError) {
+      const { error, status, message } = err;
+
+      return res.status(status).json({
+        error,
+        status,
+        message,
+      });
+    }
+
+    console.error(err);
+
+    return res.status(500).json({
+      error: true,
+      status: 500,
+      message: 'Internal server error',
+    });
+  },
 };
